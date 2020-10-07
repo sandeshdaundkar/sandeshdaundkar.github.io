ae2141a304160290581038ba981d5ffaf51c5799
Sandesh Daundkar <sandeshdaundkar@yahoo.com>
2020-10-07 20:59:55 +0530

    added plugins & integrations

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,9 +1,12 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*- #
+from datetime import datetime
 
 AUTHOR = 'Sandesh Daundkar'
 SITENAME = "Sandesh's Blog"
 SITEURL = ''
+SITETITLE = SITENAME
+SITESUBTITLE = 'Python Django Developer'
 
 PATH = 'content'
 
@@ -21,18 +24,50 @@ CATEGORY_FEED_ATOM = None
 TRANSLATION_FEED_ATOM = None
 AUTHOR_FEED_ATOM = None
 
-# Blogroll
-LINKS = (('Pelican', 'https://getpelican.com/'),
-         ('Python.org', 'https://www.python.org/'),
-         ('Jinja2', 'https://palletsprojects.com/p/jinja/'),
-         ('You can modify those links in your config file', '#'),)
 
 # Social widget
-SOCIAL = (('You can add links in your config file', '#'),
-          ('Another social link', '#'),)
+SOCIAL = (('github', 'https://github.com/sandeshdaundkar'),
+          ('stack-overflow', 'https://stackoverflow.com/users/9202733/sandeshdaundkar'),
+          ('linkedin', 'https://www.linkedin.com/in/sandesh-daundkar'),
+          ('twitter', 'https://twitter.com/sandeshdaundkar'),
+          ('facebook', 'https://www.facebook.com/sandeshdaundkr'))
 
 DEFAULT_PAGINATION = 10
 STATIC_PATHS = ['images']
 THEME = './Flex'
+PYGMENTS_STYLE = "monokai"
+
 # Uncomment following line if you want document-relative URLs when developing
 #RELATIVE_URLS = True
+
+# Menu Config
+MAIN_MENU = True
+HOME_HIDE_TAGS = True
+DISPLAY_PAGES_ON_MENU = True
+USE_FOLDER_AS_CATEGORY = False
+
+MENUITEMS = (
+    ("Categories", "/categories.html"),
+    ("Tags", "/tags.html"),
+)
+
+COPYRIGHT_YEAR = datetime.now().year
+COPYRIGHT_NAME = "Sandesh Daundkar"
+
+
+THEME_COLOR_AUTO_DETECT_BROWSER_PREFERENCE = True
+THEME_COLOR_ENABLE_USER_OVERRIDE = True
+
+USE_LESS = True
+
+CC_LICENSE = {
+    "name": "Creative Commons Attribution-ShareAlike",
+    "version": "4.0",
+    "slug": "by-sa",
+}
+
+
+# Plugins Config
+PLUGIN_PATHS = ['./plugins']
+PLUGINS = ['post_stats', 'series', 'representative_image', 'liquid_tags']
+ADD_THIS_ID = "ra-5f68af799642611f"
